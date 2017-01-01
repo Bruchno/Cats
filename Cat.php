75167ec6e3ee65d32364f3441471bcad1e0b9e69
Bruchno <bruchno@ukr.net>
2017-01-01 12:30:47 +0200

    first commit

---
+++ Cat.php
@@ -0,0 +1,22 @@
+﻿<?php
+class Animal
+{
+public $Tip = "Cat";
+public $Name;
+public $Age;
+}
+class Cat extends Animal
+{
+public function __construct( $name, $age)
+{
+	$this->Name = $name;
+	$this->Age = $age;
+	echo "Запущен конструктор с аргументами для кота по имени ";
+	echo "$this->Name <br>"; 
+}
+function miew()
+{
+	echo "Кот по имени "; echo "$this->Name"; echo " говорит МЯУ<br>";}
+}
+
+?>
---
+++ mainCat.php
@@ -0,0 +1,12 @@
+﻿<?php
+require_once "Cat.php";
+$kitty1 = new Cat("Minny", "Beby");
+$kitty2 = new Cat("Mutze", "2 Jears");
+$kitty2->miew();
+$kitty1->miew();
+echo "<br>Кошка по имени $kitty1->Name имеет возраст $kitty1->Age";
+echo "<br>Kошка по имени"; echo " $kitty2->Name"; echo " имеет возраст $kitty2->Age<br>";
+$Tusja = new Animal();
+$Nava = new Cat('Miu', 2);
+$Nava->miew();
+?>
